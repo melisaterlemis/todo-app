5b8055051d47c6dce771b08099beca7d9e9af5d0
melisaterlemis <83540629+melisaterlemis@users.noreply.github.com>
2022-12-06 16:02:33 +0300

    Added delete from database

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-//inputa girilen degerler eri tabanına kaydedildi
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("todo-input");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+function closeForm() {
+  document.getElementById("popupForm").style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+//inputa girilen degerler veri tabanina kaydedildi
 function addItem(event) {
   event.preventDefault();
-  let text = document.getElementById("todo-input");
+  let text = document.getElementById("input"); //input le değiş
   db.collection("todo-item").add({
     text: text.value,
     status: "active",
@@ -22,6 +49,20 @@ function getItems() {
     generateItems(items);
   });
 }
+
+//verileri database ve ekrandan silme işlemi
+function onClickItem(e) {
+  db.collection("todo-item")
+    .doc(e.id)
+    .delete()
+    .then(() => {
+      console.log("basarili");
+    })
+    .catch((error) => {
+      console.log("basarisiz", error);
+    });
+}
+
 // veritabanindan gelen verileri olusturuyoruz ekranda veritabnini her guncelledeigimizde yeni ogeler uretitiyoruz
 function generateItems(items) {
   let itemsHTML = "";
@@ -35,16 +76,20 @@ function generateItems(items) {
                 <img src="./assets/icon-check.svg" />
               </div>
             </div>
+            <button onclick="onClickItem(this)" data-id="${item.id}" id="${
+      item.id
+    }" class="btn"><i class="fa fa-trash"></i></button>
             <div class="todo-text ${
               item.status == "completed" ? "checked" : ""
             }">
             ${item.text}
             </div>
+           
           </div>
     `;
+    document.querySelector(".todo-items").innerHTML = itemsHTML;
+    creatEventListeners();
   });
-  document.querySelector(".todo-items").innerHTML = itemsHTML;
-  creatEventListeners();
 }
 
 function creatEventListeners() {
@@ -55,7 +100,7 @@ function creatEventListeners() {
     });
   });
 }
-//isaretlenen checkleri guncellemej
+//isaretlenen checkleri guncellemesi
 function markCompleted(id) {
   //veri tabanindan geliyor
   let item = db.collection("todo-item").doc(id);
