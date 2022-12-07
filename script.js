83e3497184763613b1264e03ef26972fc31f83d8
melisaterlemis <83540629+melisaterlemis@users.noreply.github.com>
2022-12-07 14:43:29 +0300

    modalbox update

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
 var btn = document.getElementById("todo-input");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
 btn.onclick = function () {
   modal.style.display = "block";
 };
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-function closeForm() {
-  document.getElementById("popupForm").style.display = "none";
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
 }
 
 window.onclick = function (event) {
@@ -28,12 +21,18 @@ window.onclick = function (event) {
 //inputa girilen degerler veri tabanina kaydedildi
 function addItem(event) {
   event.preventDefault();
-  let text = document.getElementById("input"); //input le değiş
+  let text = document.getElementById("input");
+  let title = document.getElementById("textInput");
+  let date = document.getElementById("dateInput");
   db.collection("todo-item").add({
+    title: title.value,
+    date: date.value,
     text: text.value,
     status: "active",
   });
   text.value = "";
+  date.value = "";
+  title.value = "";
 }
 //veritabanina eklenen degerleri aldik
 function getItems() {
@@ -79,12 +78,16 @@ function generateItems(items) {
             <button onclick="onClickItem(this)" data-id="${item.id}" id="${
       item.id
     }" class="btn"><i class="fa fa-trash"></i></button>
+    <button onclick="onClickEdit(this)" data-id="${item.id}" id="${
+      item.id
+    }" class="btn"><i class="fal fa-pencil-alt"></i></button>
             <div class="todo-text ${
               item.status == "completed" ? "checked" : ""
             }">
-            ${item.text}
+           <span class="title-class">${item.title}</span>
+           <span class="date-class">${item.date}</span> 
+           <span class="text-class">${item.text}</span> 
             </div>
-           
           </div>
     `;
     document.querySelector(".todo-items").innerHTML = itemsHTML;
